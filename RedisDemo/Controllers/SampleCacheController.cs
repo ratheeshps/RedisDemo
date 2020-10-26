0817f497a575a159db563b631b513f7fa55cfc89
ratheeshps <32994374+ratheeshps@users.noreply.github.com>
2020-10-26 10:06:30 +0530

    First Commit

---
+++ RedisDemo/Controllers/SampleCacheController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using RedisDemo.Models;
+
+namespace RedisDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleCacheController : ControllerBase
+    {
+        private static readonly List<DemoData> demoDatas = new List<DemoData>
+        {
+            new DemoData{ EmployeeName="Donald Trump", Country="USA", PassportNo="USA001", Status="True", Enrolled=DateTime.Now},
+            new DemoData{ EmployeeName="Boris Johansen", Country="UK", PassportNo="UK001", Status="True", Enrolled=DateTime.Now},
+            new DemoData{ EmployeeName="Narendara Modi", Country="India", PassportNo="IND001", Status="True", Enrolled=DateTime.Now},
+            new DemoData{ EmployeeName="Emmanuel Macron", Country="France", PassportNo="FR001", Status="True", Enrolled=DateTime.Now},
+        };
+
+        private IDistributedCache _redisCache;
+        public SampleCacheController(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+        public IActionResult Index()
+        {
+            //1. Get data from the redis cache
+            string sampleData = _redisCache.GetString("DemoData");
+
+            //if cache is not present
+            if (sampleData==null)
+            {
+                //Get Data from database here - used sample data for now.
+                sampleData = JsonSerializer.Serialize(demoDatas);
+
+                var cacheOptions = new DistributedCacheEntryOptions();
+                //Set expiry time
+                cacheOptions.SetAbsoluteExpiration(DateTimeOffset.Now.AddMinutes(2));
+                //Add data to Redis Cache
+                _redisCache.SetString("DemoData", sampleData, cacheOptions);
+            }
+            //If cache present it will return sampleData
+
+            return Ok(sampleData);
+        }
+    }
+}
---
+++ RedisDemo/Models/DemoData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisDemo.Models
+{
+    public class DemoData
+    {
+
+        public string EmployeeName { get; set; }
+        public string Country { get; set; }
+        public string PassportNo { get; set; }
+        public string Status { get; set; }
+        public DateTime Enrolled { get; set; }
+    }
+}
